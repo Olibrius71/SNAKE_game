ced0a2f1552cecd1a3cddd11f9d5ac5ba43da2d7
mesropaghumyan <mesrop.aghumyan@etu.univ-lyon1.fr>
2022-11-11 00:03:45 +0100

    implementation of function allowing to displaying or not displaying the rights on the header

--- script.js
+++ script.js
@@ -65,3 +65,56 @@ drawSquares();
 
 
 
+
+        /**********************
+         * Display 1 : Navbar *
+         **********************/
+
+// {#homeBtn} contain the home button on the navbar
+let homeBtn = document.getElementsByTagName('button').item(0);
+// {#rightsBtn} contain the rights button on the navbar
+let rightsBtn = document.getElementsByTagName('button').item(1);
+// {#playBtn} contain the play button on the navbar
+let playBtn = document.getElementsByTagName('button').item(2);
+
+
+        /**********************
+         * Display 1 : Header *
+         **********************/
+
+// {#switchBtn} is the button for switch between welcome display and rights display
+let switchBtn = document.getElementById('switch');
+// {#rightsDisplay} contain the rights display
+let rightsDisplay = document.querySelector('.rights-display');
+// {#imgSwitch} contain the scroll line image
+let imgSwitch = document.getElementsByTagName('img').item(1);
+// - top scroll line src
+    let srcTopScrollLine = 'img/ligne%20de%20défilement%20haut.png';
+// - bott scroll line src
+    let srcBottScrollLien = 'img/ligne%20de%20défilement%20bas.png';
+// {#rightsTopBtn} designate the title named 'Rights' at the top of the scroll bar
+let rightsTopbtn = document.getElementsByTagName('p').item(4);
+// {#rightsBottBtn} designate the title named 'Rights' at the bottom of the scroll bar
+let rightsBottBtn = document.getElementsByTagName('p').item(5);
+
+/**
+ * Events displaying or not displaying the rights on the header
+ */
+switchBtn.addEventListener('click', event => {
+
+    if(imgSwitch.getAttribute('src') == 'img/ligne%20de%20défilement%20bas.png') {
+        homeBtn.style.border = 'none';
+        rightsBtn.style.borderBottom = '3px solid #199E58';
+        rightsDisplay.style.display = 'flex';
+        rightsTopbtn.style.display = "none";
+        rightsBottBtn.style.display = "block";
+        imgSwitch.setAttribute('src', srcTopScrollLine);
+    } else {
+        homeBtn.style.borderBottom = '3px solid #199E58';
+        rightsBtn.style.borderBottom = 'none';
+        rightsDisplay.style.display = 'none';
+        rightsTopbtn.style.display = "block";
+        rightsBottBtn.style.display = "none";
+        imgSwitch.setAttribute('src', srcBottScrollLien);
+    }
+});
